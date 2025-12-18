4f3f342055fb429c27bc628460f551dcc05d4157
Alex Passalis <alexpassalis9@gmail.com>
2025-12-18 20:06:23 +0100

    feat: mortal java doc

--- src/app/Mortal.java
+++ src/app/Mortal.java
@@ -2,7 +2,15 @@ package app;
 
 import itumulator.world.World;
 
+/**
+ * The Mortal interface defines the behavior for entities that can be removed
+ * from the simulation, typically upon death or depletion of energy.
+ */
 public interface Mortal {
+
+    /**
+     * Removes the entity from the world.
+     */
     default void die(World world) {
         world.delete(this);
     }
